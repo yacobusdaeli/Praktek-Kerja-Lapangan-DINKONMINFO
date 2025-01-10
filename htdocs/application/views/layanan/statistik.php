8e6bd173400969d673d7a44487b202fd25810dee
s0crates_ <aldebaranyac@gmail.com>
2025-01-10 13:29:31 +0700

    Final Upload Project

--- htdocs/application/views/layanan/statistik.php
+++ htdocs/application/views/layanan/statistik.php
@@ -110,8 +110,8 @@
             datasets: [{
                 label: 'Total Permohonan',
                 data: data,
-                backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)','rgba(255, 99, 132, 0.2)',],
-                borderColor: ['rgb(255, 159, 64)','rgb(75, 192, 192)','rgb(255, 99, 132)'],
+                backgroundColor: ['rgba(75, 192, 192, 0.2)','rgba(255, 99, 132, 0.2)','rgba(255, 159, 64, 0.2)'],
+                borderColor: ['rgb(75, 192, 192)','rgb(255, 99, 132)','rgb(255, 159, 64)'],
                 borderWidth: 1
             }]
         },
